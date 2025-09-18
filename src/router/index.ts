04fb5bada392221a58b1abd26105b85ea8a0108e
David Lefrancq <david.lefrancq@gmail.com>
2025-09-18 21:57:59 +0200

    feat: Add SharedComponentsDemo and update routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,11 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
+    {
+      path: '/shared-demo',
+      name: 'shared-demo',
+      component: () => import('../components/SharedComponentsDemo.vue'),
+    },
   ],
 })
 
