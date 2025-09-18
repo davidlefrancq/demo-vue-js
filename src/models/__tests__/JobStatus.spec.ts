7b504e414d41fa1fa004e23bc794df59b7c0a0e8
David Lefrancq <david.lefrancq@gmail.com>
2025-09-18 09:59:13 +0200

    feat: JobStatus tests

---
+++ src/models/__tests__/JobStatus.spec.ts
@@ -0,0 +1,19 @@
+import { describe, it, expect } from 'vitest'
+import { JobStatus } from '../JobStatus';
+
+describe('JobStatus enum', () => {
+  it('should have correct values', () => {
+    expect(JobStatus.Applied).toBe('Applie');
+    expect(JobStatus.UnderReview).toBe('UnderReview');
+    expect(JobStatus.Shortlisted).toBe('Shortlisted');
+    expect(JobStatus.Rejected).toBe('Rejected');
+    expect(JobStatus.InterviewPlanned).toBe('InterviewPlanned');
+    expect(JobStatus.InterviewDone).toBe('InterviewDone');
+    expect(JobStatus.InterviewCanceled).toBe('InterviewCanceled');
+    expect(JobStatus.OfferMade).toBe('OfferMade');
+    expect(JobStatus.Negotiation).toBe('Negotiation');
+    expect(JobStatus.OfferAccepted).toBe('OfferAccepted');
+    expect(JobStatus.OfferDeclined).toBe('OfferDeclined');
+    expect(JobStatus.Hired).toBe('Hired');
+  });
+});
